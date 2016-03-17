d8b234d0ad8c1f0112830c52252346f5dfce445c
Ox Cart <ox.to.a.cart@gmail.com>
2016-03-17 10:43:40 -0500

    Squashed commits, full commit log available in https://github.com/getlantern/lantern

---
+++ masquerade.go
@@ -0,0 +1,16 @@
+package fronted
+
+const (
+	NumWorkers = 10 // number of worker goroutines for verifying
+)
+
+// Masquerade contains the data for a single masquerade host, including
+// the domain and the root CA.
+type Masquerade struct {
+	// Domain: the domain to use for domain fronting
+	Domain string
+
+	// IpAddress: pre-resolved ip address to use instead of Domain (if
+	// available)
+	IpAddress string
+}
